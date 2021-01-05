e052a817806b5040e8e1c9b020ad58e38e3323eb
engalgamal96 <engalgamal96@gmail.com>
2021-01-05 14:27:24 +0200

    Initial commit

--- Greedy Algorithms/Car Fueling/car_fueling.py
+++ Greedy Algorithms/Car Fueling/car_fueling.py
@@ -1,20 +1,18 @@
-# python3
-
-
 def compute_min_number_of_refills(d, m, stops):
     assert 1 <= d <= 10 ** 5
     assert 1 <= m <= 400
     assert 1 <= len(stops) <= 300
     assert 0 < stops[0] and all(stops[i] < stops[i + 1] for i in range(len(stops) - 1)) and stops[-1] < d
-    s=[]
-    x=0
-    for i in range(1,len(stops)):
-        if stops[i]-stops[i-1]>m:
+    s = []
+    x = 0
+    for i in range(1, len(stops)):
+        if stops[i] - stops[i - 1] > m:
             return -1
-        if stops[i]-x>m:
-            s.append(stops[i-1])
-            x=stops[i]
+        if stops[i] - x > m:
+            s.append(stops[i - 1])
+            x = stops[i]
     return len(s)
+
 if __name__ == '__main__':
     input_d = int(input())
     input_m = int(input())
